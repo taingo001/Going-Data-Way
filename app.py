9a1de1bb7cded903c73371390e48ecb0989359ff
taingo001 <99776178+taingo001@users.noreply.github.com>
2022-03-03 16:02:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
+import pandas as pd
+import csv
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/")
 def index():
-	flash("what's your name?")
 	return render_template("index.html")
+
+@app.route("/home")
+def index():
+	return render_template("index.html")
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+@app.route("/help")
+def help():
+	return render_template("help.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
