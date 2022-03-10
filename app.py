9e44647ebdddaa9cfd0ad3b3826034b5a0bc78f9
taingo001 <99776178+taingo001@users.noreply.github.com>
2022-03-10 15:45:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,17 +32,17 @@ def download_csv():
                      attachment_filename='csv_template.csv',
                      as_attachment=True)
 
-#@app.route("/data", methods=['GET', 'POST'])
-#def data():
-	#if request.method == 'POST':
-		#f = request.form['csv file']
-		#data = []
-		#with open(f) as file:
-			#csvfile = csv.reader(file)
-			#for row in csvfile:
-				#data.append(row)
-		#data = pd.DataFrame(data)
-		#return render_template("data.html", data=data.to_html(header=False, index=False))
+@app.route("/application", methods=['GET', 'POST'])
+def application():
+	if request.method == 'POST':
+		f = request.form['csv file']
+		data = []
+		with open(f) as file:
+			csvfile = csv.reader(file)
+			for row in csvfile:
+				data.append(row)
+		data = pd.DataFrame(data)
+		return render_template("application.html", data=data.to_html(header=False, index=False))
 
 
 @app.route("/greet", methods=['POST', 'GET'])
